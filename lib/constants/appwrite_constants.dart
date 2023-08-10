7985db11228f3452d792ca89ceee5255cc9d836b
Karan Doshi <73486482+doshikaran@users.noreply.github.com>
2023-08-10 11:14:21 -0700

    Update appwrite_constants.dart

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,9 +1,9 @@
 class AppwriteConstants {
-  static const String databaseId = '64d2f2082d77a03adea7';
-  static const String projectId = '64d2bd2ff3d5f719d729';
-  static const String endPoint = 'https://cloud.appwrite.io/v1';
+  static const String databaseId = '';
+  static const String projectId = '';
+  static const String endPoint = '';
 
-  static const String userdataCollectionId = '64d2f22520b6b729b43b';
-  static const String eventsCollectionId = '64d43e0ce55c3ec30e5d';
-  static const String buckedId = '64d4419b0fba55ae6443';
+  static const String userdataCollectionId = '';
+  static const String eventsCollectionId = '';
+  static const String buckedId = '';
 }
